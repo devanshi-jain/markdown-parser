02373f3dc471ab4f032932a409d7e8aa380422e4
Devanshi Jain <devanshijain018@gmail.com>
2022-05-02 23:32:20 -0700

    Updated Markdown

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -1,33 +1,61 @@
 //https://howtodoinjava.com/java/io/java-read-file-to-string-examples/
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
+import java.io.File;
+import java.util.Scanner;
 
 public class MarkdownParse {
+    private static final String[] VALID_CHARS = {"(", ")", "[", "]"};
+    // Found all characters that are needed to consider this line a link
 
-    public static ArrayList<String> getLinks(String markdown) {
-        ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then read link upto next )
-        int currentIndex = 0;
-        while(currentIndex < markdown.length()) {
-            int openBracket = markdown.indexOf("[", currentIndex);
-            int closeBracket = markdown.indexOf("]", openBracket);
-            int openParen = markdown.indexOf("(", closeBracket);
-            int closeParen = markdown.indexOf(")", openParen);
-            toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+
+    public static ArrayList<String> getLinks(Scanner reader) {
+        ArrayList<String> list = new ArrayList<>();
+        while (reader.hasNextLine()) {
+            String currentLine = reader.nextLine();
+            if (currentLine.length() == 0) continue;
+            // Ensure that line isn't an image
+            if (currentLine.charAt(0) == '!') continue;
+            int counter = 0;
+            for (String character : VALID_CHARS) {
+                if (currentLine.contains(character)) {
+                    counter++;
+                }
+            }
+            if (counter == 4) {
+                // Make sure that the link characters are in the correct error
+                int startSquareBracket = currentLine.indexOf("[");
+                if (startSquareBracket == -1) {
+                    continue;
+                }
+                int endSquareBracket = currentLine.indexOf("]", startSquareBracket);
+                if (endSquareBracket == -1) {
+                    continue;
+                }
+                int startParenthesis = currentLine.indexOf("(", endSquareBracket);
+                if (startParenthesis == -1) {
+                    continue;
+                }
+                int endParenthesis = currentLine.indexOf(")", startParenthesis);
+                if (endParenthesis == -1) {
+                    continue;
+                }
+                String link = currentLine.substring(startParenthesis + 1, endParenthesis);
+                list.add(link);
+            }
         }
-
-        return toReturn;
+        return list;
     }
 
 
     public static void main(String[] args) throws IOException {
-        Path fileName = Path.of(args[0]);
-        String content = Files.readString(fileName);
-        ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        if (args[0] == null) {
+            throw new IOException("No file specified");
+        }
+        File file = new File(String.valueOf(args[0]));
+        Scanner reader = new Scanner(file);
+        System.out.println(getLinks(reader));
+        reader.close();
     }
 }
